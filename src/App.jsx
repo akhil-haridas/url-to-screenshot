0725ed6879ac0d853b69eb8760a6d5e3b791f8ac
akhil-haridas <4khilharidas@gmail.com>
2024-09-05 11:41:32 +0530

    added toggle functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,23 @@
-import React from 'react'
-import './App.css'
+import React, { Fragment, useState } from "react";
+import "./App.css";
+import Html2Canvas from "./components/Html2Canvas";
 
 function App() {
-
+  const [compType, setCompType] = useState("Menu");
+  const toggleComponent = (type) => {
+    setCompType(type)
+  };
   return (
-    <div className='MenuWrapper'>
-      <button>
-        HTML 2 Canvas
-      </button>
-      <button>
-        Screenshot API
-      </button>
+    <div className="MenuWrapper">
+      {compType === "Menu" && (
+        <Fragment>
+          <button onClick={() => toggleComponent("html")}>HTML 2 Canvas</button>
+          <button onClick={() => toggleComponent("snap")}>Screenshot API</button>
+        </Fragment>
+      )}
+      {compType === "html" && <Html2Canvas toggleComponent={toggleComponent} />}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
